4331cb01bdc25c32467c632fcd110166de47ac39
rafael.tomelin <rafael.tomelin@gmail.com>
2025-06-02 17:34:16 +0000

    chore: add cors

--- pkg/http_server/server.go
+++ pkg/http_server/server.go
@@ -96,13 +96,16 @@ func (s *RestAPI) ValidateToken(c *gin.Context) {
 }
 func (api *RestAPI) CORSConfig(c *gin.Context) {
 	corsConfig = cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://studio--prosperar-n1en5.us-central1.hosted.app", "https://www.dashfin.com.br"}, // Adicione a origem do seu frontend
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-AUTHORIZATION", "X-AUTHORIZATION", "X-APP", "X-USERID", "X-TRACE-ID"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
+		AllowOrigins:  []string{"*"}, // Adicione a origem do seu frontend
+		AllowMethods:  []string{"*"},
+		AllowHeaders:  []string{"*"},
+		ExposeHeaders: []string{"Content-Length"},
+		// AllowCredentials: true,
+		MaxAge: 12 * time.Hour,
 	}
+	c.Header("Referrer-Policy", "strict-origin-when-cross-origin")
+	c.Writer.Header().Set("Content-Type", "application/json")
+
 	api.Route.Use(cors.New(corsConfig))
 	c.Next()
 }
@@ -112,26 +115,28 @@ func (s *RestAPI) CORSMiddleware(c *gin.Context) {
 	// Domínios permitidos. Para desenvolvimento, "*" pode ser usado,
 	// mas para produção, especifique o domínio do seu frontend.
 	// Ex: "https://sua-app-frontend.web.app"
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000, https://studio--prosperar-n1en5.us-central1.hosted.app, https://www.dashfin.com.br")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", "h*")
+	c.Header("Referrer-Policy", "strict-origin-when-cross-origin")
+	c.Writer.Header().Set("Content-Type", "application/json")
 
 	// Métodos HTTP permitidos
-	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
+	c.Writer.Header().Set("Access-Control-Allow-Methods", "*")
 
 	// Cabeçalhos permitidos
 	// Importante: Adicione todos os cabeçalhos personalizados que seu frontend envia.
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With, X-AUTHORIZATION,X-AUTHORIZATION, X-APP, X-USERID, X-TRACE-ID")
+	c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
 
 	// Permitir credenciais (se você usar cookies ou autenticação HTTP)
-	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+	// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
 	// Cache da preflight request (em segundos)
 	c.Writer.Header().Set("Access-Control-Max-Age", "86400") // 24 horas
 
 	// Se for uma requisição OPTIONS (preflight), retorne 204 No Content
-	if c.Request.Method == "OPTIONS" {
-		c.AbortWithStatus(http.StatusNoContent) // Ou http.StatusOK se preferir
-		return
-	}
+	// if c.Request.Method == "OPTIONS" {
+	// 	c.AbortWithStatus(http.StatusNoContent) // Ou http.StatusOK se preferir
+	// 	return
+	// }
 
 	// Processa a próxima requisição
 	c.Next()
