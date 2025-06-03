50e5e580edc9e5b69c26d20abd849f3e76b06515
rafael.tomelin <rafael.tomelin@gmail.com>
2025-06-03 15:26:44 +0000

    chore: start crypt dsta

--- pkg/cryptData/crypt.go
+++ pkg/cryptData/crypt.go
@@ -42,27 +42,18 @@ func (c *CryptData) DecodePayload(payload *string) ([]byte, error) {
 	// This assumes the token is the key and IV combined.
 
 	decodedPayload, err := base64.StdEncoding.DecodeString(*payload)
-	log.Println(err)
 	if err != nil {
 		return nil, fmt.Errorf("failed to base64 decode payload: %w", err)
 	}
-
-	log.Println("payload > ", *payload)
-	log.Println("decodedPayload > ", decodedPayload)
-	log.Println("decodedPayload string > ", string(decodedPayload))
 
 	// Assuming the token is the key and IV concatenated.
 	// In a real application, you would parse the key and IV from the token
 	// based on your token format.
 	keyAndIV, err := base64.StdEncoding.DecodeString(*c.token)
-	log.Println(err)
 	if err != nil {
 		return nil, fmt.Errorf("failed to base64 decode token for key/IV: %w", err)
 	}
 	log.Println("keyAndIV > ", string(keyAndIV))
-	// In this example, we'll assume the key is the first 16 bytes and the IV is the next 16 bytes.
-	// This is a simplification for demonstration. Your actual implementation will depend on
-	// how the key and IV are structured in your token. You might need to adjust the slicing based on your AES key size (16, 24, or 32 bytes).
 	if len(keyAndIV) < 32 {
 		log.Println(keyAndIV, len(keyAndIV), "token does not contain sufficient data for key and IV")
 		return nil, errors.New("token does not contain sufficient data for key and IV")
