a8b74713bd566e0763af3350a1fb22c78d0b7a14
rafael.tomelin <rafael.tomelin@gmail.com>
2025-06-03 12:38:04 +0000

    chore: start crypt dsta

--- pkg/cryptData/crypt.go
+++ pkg/cryptData/crypt.go
@@ -9,9 +9,6 @@ import (
 )
 
 type CryptDataInterface interface {
-	// Encode(data byte) (string, error)
-	// Decode(data *string) (byte, error)
-	// DecryptPayload(payload string) (string, error)
 	GetToken()
 }
 
@@ -46,43 +43,6 @@ func (c *CryptData) GetToken() {
 	log.Println(c.token)
 	log.Println(*c.token)
 }
-
-// func (c *CryptData) Encode(data byte) (string, error) {
-
-// 	return "", nil
-
-// }
-
-// func (c *CryptData) Decode(data *string) (byte, error) { return 0, nil }
-
-// func (c *CryptData) DecryptPayload(payload string) (string, error) {
-// 	decodedPayload, err := base64.StdEncoding.DecodeString(payload)
-// 	if err != nil {
-// 		return "", fmt.Errorf("failed to decode base64 payload: %w", err)
-// 	}
-
-// 	err = c.getStringToken()
-// 	if err != nil {
-// 		return "", fmt.Errorf("failed to decode base64 key: %w", err)
-// 	}
-
-// 	block, err := aes.NewCipher([]byte(*c.token))
-// 	if err != nil {
-// 		return "", fmt.Errorf("failed to create AES cipher: %w", err)
-// 	}
-
-// 	if len(decodedPayload)%aes.BlockSize != 0 {
-// 		return "", errors.New("ciphertext is not a multiple of the block size")
-// 	}
-
-// 	decrypted := make([]byte, len(decodedPayload))
-
-// 	for i := range decodedPayload {
-// 		decrypted[i] = decodedPayload[i] ^ decodedKey[i%len(decodedKey)]
-// 	}
-
-// 	return string(decrypted), nil
-// }
 
 func (c *CryptData) validateTokenFromString(token *string) error {
 	decodeToken, err := base64.StdEncoding.DecodeString(*token)
