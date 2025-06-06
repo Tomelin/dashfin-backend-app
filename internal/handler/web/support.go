29d1b60d421de56a5ff1c51b61486891a968c8db
rafael.tomelin <rafael.tomelin@gmail.com>
2025-06-06 16:57:28 +0000

    test with firebase auth

--- internal/handler/web/support.go
+++ internal/handler/web/support.go
@@ -52,15 +52,15 @@ func (cat *SupportHandlerHttp) handlers(routerGroup *gin.RouterGroup, middleware
 
 // Novo handler específico para OPTIONS
 func (cat *SupportHandlerHttp) HandleSupportOptions(c *gin.Context) {
-	// Os headers CORS já devem estar sendo configurados pelo seu middleware CORS global
-	// Se não, você precisaria configurá-los aqui também, por exemplo:
+
+	log.Println("recebendo o options....")
 	c.Header("Access-Control-Allow-Origin", "*") // Ou sua origem do frontend
 	c.Header("Access-Control-Allow-Methods", "*")
 	c.Header("Access-Control-Allow-Headers", "Content-Type, X-Authorization, X-USERID, X-APP, X-TRACE-ID")
 	c.Header("Access-Control-Allow-Credentials", "true")
 	c.Header("Content-Type", "application/json")
 
-	c.Status(http.StatusNoContent) // Ou http.StatusOK
+	c.Status(http.StatusNoContent)
 }
 
 // web/support_handler.go
