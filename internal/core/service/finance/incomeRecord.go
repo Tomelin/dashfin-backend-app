9fd403c54862e123559a4578292a891d3852d35b
rafael.tomelin <rafael.tomelin@gmail.com>
2025-07-02 11:56:51 +0000

    chore: add finance report frist step

--- internal/core/service/finance/incomeRecord.go
+++ internal/core/service/finance/incomeRecord.go
@@ -199,7 +199,6 @@ func (s *IncomeRecordService) GetIncomeRecords(ctx context.Context, params *enti
 		return nil, fmt.Errorf("invalid query parameters for GetIncomeRecords: %w", err)
 	}
 
-	log.Println("GetIncomeRecords query params", *queryParams.EndDate, *queryParams.StartDate)
 	records, err := s.Repo.GetIncomeRecords(ctx, queryParams)
 	if err != nil {
 		return nil, err
