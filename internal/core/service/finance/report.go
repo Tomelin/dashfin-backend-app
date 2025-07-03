e5ae9801dfba8e4d1da16edf1cd9bef425a962db
Rafael Tomelin <rafael.tomelin@gmail.com>
2025-07-03 20:54:02 +0000

    chore: update

--- internal/core/service/finance/report.go
+++ internal/core/service/finance/report.go
@@ -66,6 +66,7 @@ func (s *FinancialReportDataService) GetFinancialReportData(ctx context.Context)
 		return nil, err
 	}
 
+	return nil, nil
 	// Initialize financialReport if it is nil
 	if s.financialReport == nil {
 		s.financialReport = &entity.FinancialReportData{}
