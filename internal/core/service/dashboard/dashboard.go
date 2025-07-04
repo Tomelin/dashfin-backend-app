f820d5da34c17daad4c77b115528a8f99aa53d7c
Rafael Tomelin <rafael.tomelin@gmail.com>
2025-07-04 17:18:26 +0000

    chore: update

--- internal/core/service/dashboard/dashboard.go
+++ internal/core/service/dashboard/dashboard.go
@@ -185,7 +185,12 @@ func (s *DashboardService) getBankAccountBalance(ctx context.Context, userID *st
 	fmt.Println("\n Count expense records:", len(s.expenseRecords))
 	fmt.Println("\n Bank account balances:", balances)
 	fmt.Println("\n Count bank account balances:", len(balances))
-	for bankID := range balances {
+	count := 0
+	for bankID, val := range balances {
+		fmt.Println("\n Count is:", count)
+		count++
+		fmt.Println("\n Bank account ID:", bankID)
+		fmt.Println("\n Bank account balance:", val)
 		if bankID == "" {
 			continue
 		}
